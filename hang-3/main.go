44c380b97771df71609bb0fc539f863d96af6762
Muhammed Uluyol <uluyol0@gmail.com>
2021-02-10 12:33:46 -0500

    initial checkin

---
+++ hang-3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "net"
+
+func NotifyOnMesg(notify chan<- int) {
+	conn, _ := net.ListenPacket("udp", "127.0.0.1:9999")
+	var buf [1]byte
+	for {
+		conn.ReadFrom(buf[:])
+		notify <- 1
+	}
+}
+
+func AsyncSendMesg() <-chan int {
+	c := make(chan int)
+	go func() {
+		conn, _ := net.Dial("udp", "127.0.0.1:9999")
+		conn.Write([]byte{1})
+		c <- 1
+	}()
+	return c
+}
+
+func main() {
+	c := make(chan int)
+	go NotifyOnMesg(c)
+	<-AsyncSendMesg()
+	<-AsyncSendMesg()
+	<-c
+	<-c
+}
