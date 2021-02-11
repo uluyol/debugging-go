a1b5bc0f19bf23b7a8476328e684bc7abda43f44
Muhammed Uluyol <uluyol0@gmail.com>
2021-02-11 16:31:25 -0500

    use call() and RPCs like project code

--- hang-1/main.go
+++ hang-1/main.go
@@ -1,29 +1,85 @@
 package main
 
-import "net"
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"sync"
+)
 
-func NotifyOnMesg(notify chan<- int) {
-	conn, _ := net.ListenPacket("udp", "127.0.0.1:9999")
-	var buf [1]byte
-	conn.ReadFrom(buf[:])
-	notify <- 1
+type Server struct {
+	mu     sync.Mutex
+	notify chan int
+}
+
+func (s *Server) Notify(args *NotifyArgs, reply *NotifyReply) error {
+	s.mu.Lock()
+	s.notify <- 1
+	return nil
 }
 
 func AsyncSendMesg() <-chan int {
 	c := make(chan int)
 	go func() {
-		conn, _ := net.Dial("udp", "127.0.0.1:9998")
-		conn.Write([]byte{1})
+		go call("127.0.0.1:9998", "Server.Notify", &NotifyArgs{}, &NotifyReply{})
 		c <- 1
 	}()
 	return c
 }
 
 func main() {
-	c := make(chan int)
-	go NotifyOnMesg(c)
+	s := StartServer("127.0.0.1:9999")
 	<-AsyncSendMesg()
 	<-AsyncSendMesg()
-	<-c
-	<-c
+	<-s.notify
+	<-s.notify
 }
+
+func call(srv string, rpcname string,
+	args interface{}, reply interface{}) bool {
+	c, errx := rpc.Dial("tcp", srv)
+	if errx != nil {
+		return false
+	}
+	defer c.Close()
+
+	err := c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	log.Print(err)
+	return false
+}
+
+func StartServer(me string) *Server {
+	s := &Server{
+		notify: make(chan int),
+	}
+
+	rpcs := rpc.NewServer()
+	rpcs.Register(s)
+
+	l, e := net.Listen("tcp", me)
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+
+	// create a thread to accept RPC connections from clients.
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err == nil {
+				go rpcs.ServeConn(conn)
+			}
+			if err != nil {
+				log.Printf("Server(%v) accept: %v", me, err.Error())
+			}
+		}
+	}()
+
+	return s
+}
+
+type NotifyArgs struct{}
+type NotifyReply struct{}
